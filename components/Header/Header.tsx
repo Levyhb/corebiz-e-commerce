10a5dd97e50240f3111752198d45bc9d78bcff6b
Levy Bezerra <levybholanda@gmail.com>
2024-07-07 16:41:22 -0300

    style: header desktop

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,19 +4,38 @@ import logo from "../../public/logo.png";
 import shoppingCart from "../../public/shopping-cart.png";
 import MenuIcon from "@mui/icons-material/Menu";
 import styles from "./Header.module.css";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import { useMediaQuery } from "@mui/material";
 
 export default function Header() {
+  const isDesktop = useMediaQuery("(min-width:768px)");
+
   return (
     <header className={`${styles.header}`}>
       <div className={styles.header_wrapper}>
-        <div className={styles.menu}>
-          <MenuIcon />
-        </div>
-
+        {!isDesktop && (
+          <div className={styles.menu}>
+            <MenuIcon />
+          </div>
+        )}
         <div className={styles.logo}>
           <Image src={logo} alt="logo" />
         </div>
+
+        {isDesktop && (
+          <div className={styles.header_search}>
+            <input type="text" placeholder="O que está procurando?" />
+            <SearchIcon />
+          </div>
+        )}
+
+        {isDesktop && (
+          <div className={styles.account}>
+            <PersonOutlineIcon />
+            <span>Minha Conta</span>
+          </div>
+        )}
 
         <div className={styles.shopping_cart}>
           <Image src={shoppingCart} alt="shopping-cart" />
@@ -24,10 +43,12 @@ export default function Header() {
         </div>
       </div>
 
-      <div className={styles.header_search}>
-        <input type="text" placeholder="O que está procurando?"/>
-        <SearchIcon />
-      </div>
+      {!isDesktop && (
+        <div className={styles.header_search}>
+          <input type="text" placeholder="O que está procurando?" />
+          <SearchIcon />
+        </div>
+      )}
     </header>
   );
 }
