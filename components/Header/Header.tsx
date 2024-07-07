1acd0ecf97ddeccacd8ab87b5df63707ad49ae49
Levy Bezerra <levybholanda@gmail.com>
2024-07-07 14:35:59 -0300

    feat: Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,22 +2,31 @@ import React from "react";
 import Image from "next/image";
 import logo from "../../public/logo.png";
 import shoppingCart from "../../public/shopping-cart.png";
-import MenuIcon from '@mui/icons-material/Menu';
-import styles from "./Header.module.css"
+import MenuIcon from "@mui/icons-material/Menu";
+import styles from "./Header.module.css";
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Header() {
   return (
     <header className={`${styles.header}`}>
-      <div className={styles.menu}>
-        <MenuIcon/>
+      <div className={styles.header_wrapper}>
+        <div className={styles.menu}>
+          <MenuIcon />
+        </div>
+
+        <div className={styles.logo}>
+          <Image src={logo} alt="logo" />
+        </div>
+
+        <div className={styles.shoppingCart}>
+          <Image src={shoppingCart} alt="shopping-cart" />
+          <div className={styles.count}>1</div>
+        </div>
       </div>
 
-      <div className={styles.logo}>
-        <Image src={logo} alt="logo" />
-      </div>
-
-      <div className={styles.shoppingCart}>
-        <Image src={shoppingCart} alt="shopping-cart" />
+      <div className={styles.header_search}>
+        <input type="text" placeholder="O que está procurando?"/>
+        <SearchIcon />
       </div>
     </header>
   );
