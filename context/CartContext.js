67f34381ac9bb1e08aa10544fc4f3d160542b4f9
Levy Bezerra <levybholanda@gmail.com>
2024-07-09 14:32:08 -0300

    feat: CartContext

---
+++ context/CartContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useEffect, useReducer } from 'react';
+
+const CartContext = createContext(null);
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      const updatedCart = [...state, action.payload];
+      localStorage.setItem('cart', JSON.stringify(updatedCart));
+      return updatedCart;
+    case 'LOAD_CART':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export const CartProvider = ({ children }) => {
+  const [cart, dispatch] = useReducer(cartReducer, []);
+
+  useEffect(() => {
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+      dispatch({ type: 'LOAD_CART', payload: JSON.parse(storedCart) });
+    }
+  }, []);
+
+  return (
+    <CartContext.Provider value={{ cart, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
