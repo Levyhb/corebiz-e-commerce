fe7b273bce83e8c1eb670aac17ca4573cf17591f
Levy Bezerra <levybholanda@gmail.com>
2024-07-09 14:41:50 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { MyProvider } from "../context/MyContext";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import { CartProvider } from "@/context/CartContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MyProvider>
+    <CartProvider>
       <Header />
       <main>
         <Component {...pageProps} />
       </main>
       <Footer />
-    </MyProvider>
+    </CartProvider>
   );
 }
